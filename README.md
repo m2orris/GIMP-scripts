21bdc8e2a54ffc0f01f2c989d9c021536158d38b
Michael Morris <mbmorrisjr@gmail.com>
2013-04-17 16:15:57 -0500

    Generalized the single icon export. Retina and/or Standard images can be exported in a single step. Square scaling 1 for 1 width and height. Rectangular scaling that preserves the original image's proportions. Cleaner interface. Saving png background and gamma attributes. Using gimp-image-merge-visible-layers to combine layers instead of gimp-image-flatten. Updated documenation. Removed .gitignore

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #GIMP-scripts
-###Version 1.0
+###Version 1.10
 #####Copyright (c) 2013 Michael Morris<br>This software is released under MIT Open Source License
 
 **************
@@ -12,7 +12,7 @@
 
 ###Script-Fu / export_iOS_icons_of_image.scm###
 
-This Script-Fu script automatically generates all the required app icons with the proper names and resolutions for the various iOS devices from a single source image.
+This Script-Fu script automatically generates all the required and recommend app icons with the proper names and resolutions for the various iOS devices from a single source image.
 
 For each app icon: the source image* is duplicated, then the duplicated image is scaled, then the scaled duplicate image is exported to a .png file, and finally the scaled duplicate image is deleted. The resulting .png files have the proper names and resolutions for iOS app icons for iPad and iPhone/iPod touch for both normal and retina displays.
 
@@ -20,8 +20,8 @@ For each app icon: the source image* is duplicated, then the duplicated image is
 
 This script contains two functions.
 
-* **Function Name:** `export-ios-icons-of-image-for-device`<br>
-**Menu:** `File --> Export iOS Icon of Image --> Icons for Device(s) ...`<br>
+* **Function Name:** `export-as-ios-icons-for-device`<br>
+**Menu:** `File --> Export as iOS --> Icons for Device(s) ...`<br>
 Exports multiple iOS app icons to a user specified directory for a user specified iOS device. The following are the possible iOS app icon filenames (and resolutions):<br>
 `iTunesArtwork@2x (1024 x 1024)`<br>
 `iTunesArtwork (512 x 512)`<br>
@@ -34,11 +34,23 @@ Exports multiple iOS app icons to a user specified directory for a user specifie
 `Icon-Small@2x.png (58 x 58)`<br>
 `Icon-Small.png (29 x 29)`<br>
 
-* **Function Name:** `export-ios-icon-of-image`<br>
-**Menu:** `File --> Export iOS Icon of Image --> Single Icon ...`<br>
-Exports a single iOS app icon to a user specified directory with a user specified filename and a user specified width in pixels.
-
+* **Function Name:** `export-as-ios-retina-and-standard-image`<br>
+**Menu:** `File --> Export as iOS --> Image(s) ...`<br>
+Export scaled (selectable) iOS image(s) (retina and/or standard) from the active image.  The scaling is either square or rectangular. Square scaling results in iOS images that have the width and height equal to the size specified. Rectangular scaling results in iOS image(s) that have either the width or height equal to the size specified and the other dimension scaled proportionally.
 ##Installation
 1. Copy the scripts from the `GIMP-scripts/Script-Fu` directory to the GIMP scripts directory.
 2. Start GIMP
 3. In GIMP `Filters --> Script-Fu --> Refresh Script`
+##Release Notes
+### 1.10 - April 17, 2013
+* Generalized the single icon export.
+  * Retina and/or Standard images can be exported in a single step.
+  * Square scaling 1 for 1 width and height.
+  * Rectangular scaling that preserves the original image's proportions.
+  * Cleaner interface.
+* Saving png background and gamma attributes. 
+* Using `gimp-image-merge-visible-layers` to combine layers instead of `gimp-image-flatten`.
+### 1.00 - January 17, 2013
+* Initial release
+##Contributors
+* **Eric Genet** - iOS Image Export (Retina & Standard), png file saving tweaks, `gimp-image-merge-visible-layers`, and rectangular scaling.
--- Script-Fu/export_iOS_icons_of_image.scm
+++ Script-Fu/export_iOS_icons_of_image.scm
@@ -3,21 +3,21 @@
 ; This software is released under MIT Open Source License
 ; ==============================================================================
 
-(define (export-ios-icon-of-image inImage inDrawable inPath inFilename inDimension)
+; ------------------------------------------------------------------------------
+(define (export-as-ios-image inImage inDrawable inPath inFilename inWidth inHeight)
     (let* (
         (thePathAndFilename (string-append inPath DIR-SEPARATOR inFilename)))
         
         ; Check to make sure thePathAndFilename does not exist
         (if (file-exists? thePathAndFilename)
         
-            ; The file exists, display message and exit
+            ; The file exists, then display message and exit
             (gimp-message (string-append "The file:[" thePathAndFilename "] exists, skipping."))
             
-            ; The file does not exist, duplicate, scale, export, and delete duplicate
-            (let (
+            ; The file does not exist, then duplicate, scale, export, and delete duplicate
+            (let* (
                 (theDuplicateImage (car (gimp-image-duplicate inImage))))
-                
-               (gimp-image-scale theDuplicateImage inDimension (* (car (gimp-image-height theDuplicateImage)) (/ inDimension (car (gimp-image-width theDuplicateImage)))))
+                (gimp-image-scale theDuplicateImage inWidth inHeight)
                 (file-png-save
                     RUN-NONINTERACTIVE
                     theDuplicateImage
@@ -31,94 +31,113 @@
                     0               ; Write oFFs chunk?
                     0               ; Write pHYs chunk?
                     0)              ; Write tIME chunk?
-                (gimp-image-delete theDuplicateImage)
-            )
-        )
-    )
-)
+                (gimp-image-delete theDuplicateImage)))))
 
+; ------------------------------------------------------------------------------
+(define (export-as-ios-retina-and-standard-image inImage inDrawable inPath inFilename inImageExportOption inImageScalingOption inSize)
+    ; Image export: retina
+    (if (or (= 0 inImageExportOption) (= 1 inImageExportOption))
+        (let* (
+            (filename (string-append inFilename "@2x.png")))
+          
+            (if (= 0 inImageScalingOption)
+                ; Scaling option square: width = size, height = size
+                (export-as-ios-image inImage inDrawable inPath filename inSize inSize)
+
+                (if (= 1 inImageScalingOption)
+                    ; Scaling option rectangle: width = size, height is proportionate
+                    (export-as-ios-image inImage inDrawable inPath filename inSize (round (* (car (gimp-image-height inImage)) (/ inSize (car (gimp-image-width inImage))))))
+
+                    (if (= 2 inImageScalingOption)
+                        ; Scaling option rectangle: width is proportionate, height = size
+                        (export-as-ios-image inImage inDrawable inPath filename (round (* (car (gimp-image-width inImage)) (/ inSize (car (gimp-image-height inImage))))) inSize))))
+            ; Halve the image size for standard image
+            (set! inSize (round (/ inSize 2)))))
+
+    ; Image export: standard
+    (if (or (= 0 inImageExportOption) (= 2 inImageExportOption))
+        (let* (
+            (filename (string-append inFilename ".png")))
+            
+            (if (= 0 inImageScalingOption)
+                ; Scaling option square: width = size, height = size
+                (export-as-ios-image inImage inDrawable inPath filename inSize inSize)
+
+                (if (= 1 inImageScalingOption)
+                    ; Scaling option rectangle: width = size, height is proportionate
+                    (export-as-ios-image inImage inDrawable inPath filename inSize (round (* (car (gimp-image-height inImage)) (/ inSize (car (gimp-image-width inImage))))))
+
+                    (if (= 2 inImageScalingOption)
+                        ; Scaling option rectangle: width is proportionate, height = size
+                        (export-as-ios-image inImage inDrawable inPath filename (round (* (car (gimp-image-width inImage)) (/ inSize (car (gimp-image-height inImage))))) inSize)))))))
+
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 (script-fu-register
-    "export-ios-icon-of-image"                  ;func name
-    "Single Icon ..."                           ;menu label
-    "Exports an iOS icon of the image around the user's choice of:\
-      path, filename, and width in pixels."     ;description           
-    "Michael Morris"                            ;author
+    "export-as-ios-retina-and-standard-image"                       ;func name
+    "Image(s) ..."                                                  ;menu label
+    "Export scaled (selectable) iOS image(s) (retina and/or\
+    standard) from the active image.  The scaling is either square\
+    or rectangular.\
+    \
+    Square scaling results in iOS images that have the width and\
+    height equal to the size specified.\
+    \
+    Rectangular scaling results in iOS image(s) that have either\
+    the width or height equal to the size specified and the other\
+    dimension scaled proportionally."                               ;description
+    "Michael Morris"                                                ;author
     "Copyright (c) 2013 Michael Morris\         
-      This software is released under MIT Open Source License" ;copyright notice
-    "January 10, 2013"                          ;date created
-    "*"                                         ;image type that the script works on
-    SF-IMAGE     "Image"            0
-    SF-DRAWABLE  "Drawable"         0
-    SF-DIRNAME   "Path"             "/tmp"
-    SF-STRING    "Filename"         "Icon.png"
-    SF-VALUE     "Width in Pixels"  "57"
+    This software is released under MIT Open Source License"        ;copyright notice
+    "April 17, 2013"                                                ;date created
+    "*"                                                             ;image type that the script works on
+    SF-IMAGE     "Image"                                    0
+    SF-DRAWABLE  "Drawable"                                 0
+    SF-DIRNAME   "Path"                                     "/tmp"
+    SF-STRING    "Filename\n  (without @2x.png & .png)"     "Image"
+    SF-OPTION    "Image(s) exported" '("Retina and Standard" "Retina only" "Standard only")
+    SF-OPTION    "Image scaling" '("Square (width = size, height = size)" "Rectangle (width = size, height is proportionate)" "Rectangle (width is proportionate, height = size)")
+    SF-VALUE     "Size of image in Pixels\n  (if both retina and standard\n  images are being exported,\n  the standard image size\n  will be half of the value)"   "114"
 )
 
-(script-fu-menu-register "export-ios-icon-of-image" "<Image>/File/iOS Icon & Image exports")
+(script-fu-menu-register "export-as-ios-retina-and-standard-image" "<Image>/File/Export as iOS")
 
-(define (export-ios-retina-of-image inImage inDrawable inPath inFilename inDimension)
- 
-            (export-ios-icon-of-image inImage inDrawable inPath (string-append (string-append inFilename "@2x") ".png") inDimension)
-            (export-ios-icon-of-image inImage inDrawable inPath (string-append inFilename ".png") (/ inDimension 2))
-
-)
-
-(script-fu-register
-    "export-ios-retina-of-image"                                        ;func name
-    "Standard and Retina version for image"                             ;menu label
-    "Exports an image in retina and standard format with the user's choice of:\
-      path, filename, and width in pixels."                             ;description           
-    "Michael Morris"                                                    ;author
-    "Copyright (c) 2013 Michael Morris\         
-      This software is released under MIT Open Source License"          ;copyright notice
-    "April, 16   2013"                                                  ;date created
-    "*"                                                                 ;image type that the script works on
-    SF-IMAGE     "Image"            0
-    SF-DRAWABLE  "Drawable"         0
-    SF-DIRNAME   "Path"             "/tmp"
-    SF-STRING    "Filename"         "Icon"
-    SF-VALUE     "Width in Pixels"  "57"
-)
-
-(script-fu-menu-register "export-ios-retina-of-image" "<Image>/File/iOS Icon & Image exports")
-
-; ==============================================================================
-
-(define (export-ios-icons-of-image-for-device inImage inDrawable inPath iniPadIcons iniPhoneIcons)
+; ------------------------------------------------------------------------------
+(define (export-as-ios-icons-for-device inImage inDrawable inPath iniPadIcons iniPhoneIcons)
     
     (if (or (= 1 iniPadIcons) (= 1 iniPhoneIcons))
         (begin
-            (export-ios-icon-of-image inImage inDrawable inPath "iTunesArtwork@2x" 1024)
-            (export-ios-icon-of-image inImage inDrawable inPath "iTunesArtwork" 512)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-Small@2x.png" 58)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-Small.png" 29)))
+            (export-as-ios-image inImage inDrawable inPath "iTunesArtwork@2x" 1024 1024)
+            (export-as-ios-image inImage inDrawable inPath "iTunesArtwork" 512 512)
+            (export-as-ios-image inImage inDrawable inPath "Icon-Small@2x.png" 58 58)
+            (export-as-ios-image inImage inDrawable inPath "Icon-Small.png" 29 29)))
     (if (= 1 iniPadIcons)
         (begin
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-72@2x.png" 144)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-72.png" 72)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-Small-50@2x.png" 100)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon-Small-50.png" 50)))
+            (export-as-ios-image inImage inDrawable inPath "Icon-72@2x.png" 144 144)
+            (export-as-ios-image inImage inDrawable inPath "Icon-72.png" 72 72)
+            (export-as-ios-image inImage inDrawable inPath "Icon-Small-50@2x.png" 100 100)
+            (export-as-ios-image inImage inDrawable inPath "Icon-Small-50.png" 50 50)))
     (if (= 1 iniPhoneIcons)
         (begin
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon@2x.png" 114)
-            (export-ios-icon-of-image inImage inDrawable inPath "Icon.png" 57)))
-)
+            (export-as-ios-image inImage inDrawable inPath "Icon@2x.png" 114 114)
+            (export-as-ios-image inImage inDrawable inPath "Icon.png" 57 57))))
 
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 (script-fu-register
-    "export-ios-icons-of-image-for-device"      ;func name
-    "Icons for Device(s) ..."                   ;menu label
-    "Exports iOS icons of the image around the user's choice of:\
-      path and device(s): iPad and/or iPhone/iPod touch";description        
-    "Michael Morris"                            ;author
+    "export-as-ios-icons-for-device"                                ;func name
+    "Icons for Device(s) ..."                                       ;menu label
+    "Exports the required and recommend iOS icons for the iOS\
+    devices (selectable) and iOS display types from the active\
+    image."                                                         ;description
+    "Michael Morris"                                                ;author
     "Copyright (c) 2013 Michael Morris\
-      This software is released under MIT Open Source License" ;copyright notice
-    "January 10, 2013"                          ;date created
-    "*"                                         ;image type that the script works on
-    SF-IMAGE     "Image"                           0
-    SF-DRAWABLE  "Drawable"                        0
-    SF-DIRNAME   "Path"                            "/tmp"
-    SF-TOGGLE    "Create iPad icons"               1
-    SF-TOGGLE    "Create iPhone/iPod touch icons"  1
+    This software is released under MIT Open Source License"        ;copyright notice
+    "April 17, 2013"                                                ;date created
+    "*"                                                             ;image type that the script works on
+    SF-IMAGE    "Image"                             0
+    SF-DRAWABLE "Drawable"                          0
+    SF-DIRNAME  "Path"                              "/tmp"
+    SF-TOGGLE   "Create iPad icons"                 1
+    SF-TOGGLE   "Create iPhone/iPod touch icons"    1
 )
 
-(script-fu-menu-register "export-ios-icons-of-image-for-device" "<Image>/File/iOS Icon & Image exports")
+(script-fu-menu-register "export-as-ios-icons-for-device" "<Image>/File/Export as iOS")
